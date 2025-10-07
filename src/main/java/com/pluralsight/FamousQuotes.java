69855f095625b08e23b9126ce5714f5c0811274d
jasonli <lijason991@gmail.com>
2025-10-07 09:50:34 -0400

    rudimentary do/while

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,9 +1,57 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FamousQuotes {
+
+    public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        String[] quotes = {
+                "The only limit to our realization of tomorrow is our doubts of today. – Franklin D. Roosevelt",
+                "In the middle of difficulty lies opportunity. – Albert Einstein",
+                "Be yourself; everyone else is already taken. – Oscar Wilde",
+                "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
+                "The best way to predict the future is to invent it. – Alan Kay",
+                "Do what you can, with what you have, where you are. – Theodore Roosevelt",
+                "Life is what happens when you're busy making other plans. – John Lennon",
+                "You miss 100% of the shots you don’t take. – Wayne Gretzky",
+                "Whether you think you can or you think you can’t, you’re right. – Henry Ford",
+                "The journey of a thousand miles begins with one step. – Lao Tzu"
+        };
+
+        boolean isRunning = true;
+
+        do {
+
+        System.out.println("Please pick a number from 1-10 to get a quote!");
+        int quoteNum = input.nextInt();
+
+            System.out.println(quotes[quoteNum - 1]);
+
+            System.out.println("""
+                    Would you like to hear another quote?
+                    (1) Yes
+                    (2) Random quote
+                    (3) Exit program
+                    """);
+
+            int option = input.nextInt();
+
+            switch (option) {
+                case 1:
+                    break;
+                case 2:
+                    quoteNum = (int)(Math.random() * 10 + 1);
+                    break;
+                case 3:
+                    System.out.println("EXITING....");
+                    isRunning = false;
+                    break;
+            }
+
+        } while (isRunning);
 
 
 
